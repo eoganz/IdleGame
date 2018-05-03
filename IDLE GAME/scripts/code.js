c0703b992e0e9861ec57d8fef6d6e4ad9c1f58a0
eoganz <36274709+eoganz@users.noreply.github.com>
2018-05-03 06:58:56 -0700

    Add files via upload
    Mostly aesthetic with a little to the code.

--- IDLE GAME/scripts/code.js
+++ IDLE GAME/scripts/code.js
@@ -2,12 +2,14 @@ var Game = (function () {
     function Game() {
         this.gold = 0;
         this.clickGold = 1;
-        this.farms = 0;
         this.tickSpeed = 1000;
         this.assistants = 0;
-        this.factionFindChance = 10;
         this.buildingCount = 0;
+        this.factionFindChance = 10;
         this.factionCoins = new Array(0, 0, 0, 0, 0, 0);
+        this.farms = new Array(10, 0, 2);
+        this.inns = new Array(125, 0, 6);
+        this.blacksmiths = new Array(600, 0, 20);
     }
     return Game;
 }());
@@ -15,16 +17,37 @@ $(function () {
 });
 var game = new Game();
 function goldClick() {
-    var findCoin = false;
+    game.gold += game.clickGold;
     var clickChance = Math.floor((Math.random() * 100) + 1);
     var coinDecider = Math.floor((Math.random() * 5));
     if (clickChance < game.factionFindChance) {
         game.factionCoins[coinDecider] += 1;
-        updateFactionCoins();
     }
-    game.gold += game.clickGold;
-    $('#gold').text(game.gold);
+    updateGame();
 }
+function buildingTick(buildingProduction) {
+    game.gold += buildingProduction;
+    updateGame();
+}
+function buyBuilding(building) {
+    if (game.gold > building[0]) {
+        building[1] += 1;
+        game.gold -= building[0];
+        game.buildingCount += 1;
+        building[0] = Math.floor(building[0] * Math.pow(1.1, building[1]));
+    }
+    updateGame();
+}
+function test() {
+    console.log(game.factionCoins);
+    console.log(game.farms);
+}
+window.setInterval(function () {
+    var totalProd = getTotalProduction();
+    if (totalProd > 0) {
+        buildingTick(totalProd);
+    }
+}, game.tickSpeed);
 function updateFactionCoins() {
     $('#angelCoins').text(game.factionCoins[0]);
     $('#fairyCoins').text(game.factionCoins[1]);
@@ -33,23 +56,36 @@ function updateFactionCoins() {
     $('#undeadCoins').text(game.factionCoins[4]);
     $('#goblinCoins').text(game.factionCoins[5]);
 }
-function buyFarm() {
-    var farmCost = Math.floor(10 * Math.pow(1.1, game.farms));
-    if (game.gold > farmCost) {
-        game.farms += 1;
-        game.gold -= farmCost;
-        $('#farms').text(game.farms);
-        $('#gold').text(game.gold);
-    }
-    game.buildingCount += 1;
-    var nextFarmCost = Math.floor(10 * Math.pow(1.1, game.farms));
-    $('#farmCost').text(nextFarmCost);
+function updateGold() {
+    $('#gold').text(game.gold);
 }
-function test() {
-    console.log(game.factionCoins);
+function updateAllBuildings() {
+    $('#farms').text(game.farms[1]);
+    $('#farmCost').text(game.farms[0]);
+    $('#farmProduction').text(game.farms[2] * game.farms[1]);
+    $('#inns').text(game.inns[1]);
+    $('#innCost').text(game.inns[0]);
+    $('#innProduction').text(game.inns[2] * game.inns[1]);
+    $('#blacksmiths').text(game.blacksmiths[1]);
+    $('#blacksmithCost').text(game.blacksmiths[0]);
+    $('#blacksmithProduction').text(game.blacksmiths[2] * game.blacksmiths[1]);
 }
-window.setInterval(function () {
-    if (game.farms > 0) {
-        goldClick();
+function updateGame() {
+    updateFactionCoins();
+    updateGold();
+    updateAllBuildings();
+    $('#totalProduction').text(getTotalProduction());
+}
+function getTotalProduction() {
+    var totalProd = 0;
+    if (game.farms[1] > 0) {
+        totalProd += game.farms[2] * game.farms[1];
     }
-}, game.tickSpeed);
+    if (game.inns[1] > 0) {
+        totalProd += game.inns[2] * game.inns[1];
+    }
+    if (game.blacksmiths[1] > 0) {
+        totalProd += game.blacksmiths[2] * game.blacksmiths[1];
+    }
+    return totalProd;
+}
